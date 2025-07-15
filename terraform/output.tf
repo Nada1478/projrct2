3bc0caf1199350cc2be8966335a55eeaf9474034
Nada Ahmed <nadaahmedsaad839@gmail.com>
2025-07-16 00:48:04 +0300

    Add terraf

--- terraform/output.tf
+++ terraform/output.tf
@@ -1,5 +1,4 @@
-output "wordpress_instance_public_ip" {
-  value = aws_instance.wordpressserver.public_ip
+output "wordpress_public_ip" {
+  value       = aws_instance.wordpressserver.public_ip
   description = "Public IP address of the WordPress EC2 instance"
 }
-
